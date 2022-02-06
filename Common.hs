6bbbfd83b8f5f36d61cff4d2166b2ded8dd2eb40
Joshua Barrass <josh.barrass.work@gmail.com>
2022-02-06 00:54:10 +0000

    Move code to Common module for use in solver

---
+++ Common.hs
@@ -0,0 +1,46 @@
+module Common
+  ( Result(..)
+  , loadDictionary
+  , chooseWord
+  , check
+  , removeFirst
+  ) where
+
+import System.Random
+
+data Result = Wrong | Position | Correct | Unknown deriving (Show, Eq)
+
+loadDictionary :: FilePath -> IO [String]
+loadDictionary fp = do
+  contents <- readFile fp
+  return $ lines contents
+
+chooseWord :: [String] -> IO String
+chooseWord dict = do
+  gen <- getStdGen
+  let (i, _) = randomR (0, length dict - 1) gen
+  return $ dict !! i
+
+removeFirst :: (Eq a) => a -> [a] -> [a]
+removeFirst _ [] = []
+removeFirst x (y:xs)
+  | y == x = xs
+  | otherwise = y : removeFirst x xs
+
+isCorrect :: Char -> Char -> (Char, Result)
+isCorrect a b = if a == b then (b, Correct) else (b, Unknown)
+
+checkLoop [] _ = []
+checkLoop xs [] = [if x == Unknown then Wrong else x | (_,x) <- xs]
+checkLoop ((_, Correct):xs) remaining = Correct : checkLoop xs remaining
+checkLoop ((c, Unknown):xs) remaining = let
+  isValid = c `elem` remaining
+  toInsert = if isValid then Position else Wrong
+  newRemaining = if isValid then removeFirst c remaining else remaining
+  in toInsert : checkLoop xs newRemaining
+
+check :: String -> String -> [Result]
+check true guess = let
+  exact = zipWith isCorrect true guess
+  remaining = map fst $ filter (\x -> snd (snd x) /= Correct) $ zip true exact
+  in checkLoop exact remaining
--- play.hs
+++ play.hs
@@ -1,30 +1,4 @@
-import System.Random
-
-data Result = Wrong | Position | Correct | Unknown deriving (Show, Eq)
-
-removeFirst :: (Eq a) => a -> [a] -> [a]
-removeFirst _ [] = []
-removeFirst x (y:xs)
-  | y == x = xs
-  | otherwise = y : removeFirst x xs
-
-isCorrect :: Char -> Char -> (Char, Result)
-isCorrect a b = if a == b then (b, Correct) else (b, Unknown)
-
-checkLoop [] _ = []
-checkLoop xs [] = [if x == Unknown then Wrong else x | (_,x) <- xs]
-checkLoop ((_, Correct):xs) remaining = Correct : checkLoop xs remaining
-checkLoop ((c, Unknown):xs) remaining = let
-  isValid = c `elem` remaining
-  toInsert = if isValid then Position else Wrong
-  newRemaining = if isValid then removeFirst c remaining else remaining
-  in toInsert : checkLoop xs newRemaining
-
-check :: String -> String -> [Result]
-check true guess = let
-  exact = zipWith isCorrect true guess
-  remaining = map fst $ filter (\x -> snd (snd x) /= Correct) $ zip true exact
-  in checkLoop exact remaining
+import Common
 
 oneRound :: Int -> String -> IO [Result]
 oneRound round correct = do
@@ -56,17 +30,6 @@ playGame correct round = do
                putStr "\n"
                playGame correct $ round+1
            )
-
-loadDictionary :: FilePath -> IO [String]
-loadDictionary fp = do
-  contents <- readFile fp
-  return $ lines contents
-
-chooseWord :: [String] -> IO String
-chooseWord dict = do
-  gen <- getStdGen
-  let (i, _) = randomR (0, length dict - 1) gen
-  return $ dict !! i
 
 main = do
   dict <- loadDictionary "words.txt"
