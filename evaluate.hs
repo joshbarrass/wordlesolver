578c5435010cb5c4df8435d0af436ca6787cf8b2
Joshua Barrass <josh.barrass.work@gmail.com>
2022-02-13 02:21:17 +0000

    Evaluate the new solver

--- evaluate.hs
+++ evaluate.hs
@@ -2,15 +2,22 @@ import Text.Printf
 import Data.Foldable
 
 import Common
-import Solver.Probability
+import Solver.HardOptimal
 
-autoSolve :: [String] -> String -> [[Result]]
-autoSolve [] _ = []
-autoSolve dict word = let
-  guess = getNextGuess dict
+autoSolve :: [String] -> Int -> String -> [[Result]]
+autoSolve [] _ _ = []
+autoSolve dict 1 word = let
+  guess = "arise"
   result = check word guess
-  newDict = filterDict guess result dict
-  in if all (==Correct) result then [result] else result : autoSolve newDict word
+  -- newDict = filterDict guess result dict
+  newDict = dict
+  in if all (==Correct) result then [result] else result : autoSolve newDict 2 word
+autoSolve dict i word = let
+  guess = getNextGuess dict 
+  result = check word guess
+  -- newDict = filterDict guess result dict
+  newDict = dict
+  in if all (==Correct) result then [result] else result : autoSolve newDict (i+1) word
 
 mean :: (Foldable t, Real a, Fractional a) => t a -> a
 mean xs = let
@@ -18,14 +25,19 @@ mean xs = let
 
 main = do
   dict <- loadDictionary "words.txt"
-  let sols = map (autoSolve dict) dict
-  let solved = filter (all (==Correct) . last) sols
-  let solvedInTime = filter ((<= 6) . length) solved
-  let guessesNeeded = map length solvedInTime
+  let sols = map (\x -> (x, autoSolve dict 1 x)) dict
+  let solved = filter (all (==Correct) . last . snd) sols
+  let solvedInTime = filter ((<= 6) . length . snd) solved
+  let guessesNeeded = map (length . snd) solvedInTime
   putStrLn $ "Total words: " ++ show (length dict)
   let percentSolved = fromIntegral (length solvedInTime) / fromIntegral (length dict) * 100 :: Double
   let meanGuesses = (mean $ map fromIntegral guessesNeeded) :: Double
   putStrLn $ "Number solved: " ++ show (length solvedInTime) ++ " (" ++ printf "%.2f" percentSolved ++ "%)"
   putStrLn $ "Average guesses needed: " ++ printf "%.2f" meanGuesses
   putStrLn $ "Max guesses needed: " ++ show (length $ maximumBy (\x y -> length x `compare` length y) solvedInTime)
+
+  let solvedWords = map fst solvedInTime
+  let failureWords = filter (`notElem` solvedWords) dict
+  print failureWords
+  
   return ()
