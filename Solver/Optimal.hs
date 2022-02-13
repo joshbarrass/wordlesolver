ca9dbd22984a7ab757346b856f3a9ed3d3843d7f
Joshua Barrass <josh.barrass.work@gmail.com>
2022-02-13 01:40:05 +0000

    Significant optimisation -- ~5s with -O2

--- Solver/Optimal.hs
+++ Solver/Optimal.hs
@@ -1,21 +1,10 @@
 module Solver.Optimal
   ( getNextGuess
-  , resultIndex
-  , findWorstCase
   ) where
 
 import Common
 import Data.Foldable
-
-allResults :: [[Result]]
-allResults = let
-  x = [Correct, Position, Wrong]
-  in [[a,b,c,d,e] | a <- x, b <- x, c <- x, d <- x, e <- x]
-
--- findWorstCase :: [String] -> String -> Int
--- findWorstCase dict word = let
---   allFiltered = map (\result -> filterDict word result dict) allResults
---   in length $ maximumBy (\x y -> length x `compare` length y) allFiltered
+import Data.Map (Map, fromList, adjust)
 
 checkAll :: [String] -> String -> [[Result]]
 checkAll dict word = map (`check` word) dict
@@ -27,13 +16,11 @@ resultIndex xs = sum [(case x of
                              Wrong -> 2
                   ) * (3^i) | (i, x) <- zip [0..] xs]
 
-binResults :: [[Result]] -> [Int]
-binResults [] = [0 | _ <- [0.. (resultIndex [Wrong, Wrong, Wrong, Wrong, Wrong])]]
+binResults :: [[Result]] -> Map Int Int
+binResults [] = fromList [(i, 0) | i <- [0.. (resultIndex [Wrong, Wrong, Wrong, Wrong, Wrong])]]
 binResults (x:xs) = let
-  p = binResults xs
   i = resultIndex x
-  xi = p !! i
-  in take i p ++ (xi + 1) : drop (i+1) p
+  in adjust (+1) i (binResults xs)
 
 findWorstCase :: [String] -> String -> Int
 findWorstCase dict word = let
